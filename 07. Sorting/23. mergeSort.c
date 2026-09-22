3537e5146c12d6e9727cfb4acb90d27b48c021b0
Ana. <anamikadvnth80@gmail.com>
2026-09-22 19:46:49 +0530

    Add merge sort implementation in mergeSort.c
    Implement merge sort algorithm to sort an array.

--- 07. Sorting/23. mergeSort.c
+++ 07. Sorting/23. mergeSort.c
@@ -1,1 +1,80 @@
+/*
+ * Program No.: 23
+ * Program Name: Merge sort
+ * Date: 07.09.26
+ * Aim: Sort an array using merge sort.
+ */
+#include <stdio.h>
 
+void merge(int arr[], int p, int q, int r) {
+    int n1 = q - p + 1;
+    int n2 = r - q;
+    int L[n1], M[n2];
+    
+    for (int i = 0; i < n1; i++) {
+        L[i] = arr[p + i];
+    }
+    for (int j = 0; j < n2; j++) {
+        M[j] = arr[q + 1 + j];
+    }
+    
+    int i = 0, j = 0, k = p;
+    while (i < n1 && j < n2) {
+        if (L[i] <= M[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = M[j];
+            j++;
+        }
+        k++;
+    }
+    
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    
+    while (j < n2) {
+        arr[k] = M[j];
+        j++;
+        k++;
+    }
+}
+
+void mergesort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        mergesort(arr, l, m);
+        mergesort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {38, 27, 43, 3, 9, 82, 10};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    
+    printf("Original array: ");
+    printArray(arr, size);
+    
+    mergesort(arr, 0, size - 1);
+    
+    printf("Array after merge sort: ");
+    printArray(arr, size);
+    return 0;
+}
+/*
+ * Output:
+ * Enter number of elements: 7
+ * Enter 7 elements: 38 27 43 3 9 82 10
+ * Array after merge sort: 3 9 10 27 38 43 82
+ */
