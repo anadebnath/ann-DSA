d01fadceb2a901816e2dbdb48fdf9ae9a733c220
Ana. <anamikadvnth80@gmail.com>
2026-09-22 19:43:54 +0530

    Add quick sort implementation in quickSort.c
    Implement quick sort algorithm to sort an array.

--- 07. Sorting/21. quickSort.c
+++ 07. Sorting/21. quickSort.c
@@ -1,1 +1,54 @@
+/*
+ * Program No.: 21
+ * Program Name: Quick sort
+ * Date: 31.08.26
+ * Aim: Sort an array using quick sort.
+ */
+#include <stdio.h>
 
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int array[], int low, int high) {
+    int pivot = array[high];
+    int i = (low - 1);
+    for (int j = low; j < high; j++) {
+        if (array[j] <= pivot) {
+            i++;
+            swap(&array[i], &array[j]);
+        }
+    }
+    swap(&array[i + 1], &array[high]);
+    return (i + 1);
+}
+
+void quickSort(int array[], int low, int high) {
+    if (low < high) {
+        int pi = partition(array, low, high);
+        quickSort(array, low, pi - 1);
+        quickSort(array, pi + 1, high);
+    }
+}
+
+int main() {
+    int data[] = {6, 4, 7, 8, 1, 9, 5};
+    int size = sizeof(data) / sizeof(data[0]);
+    
+    quickSort(data, 0, size - 1);
+    
+    printf("Sorted Array\n");
+    for (int i = 0; i < size; ++i) {
+        printf("%d ", data[i]);
+    }
+    printf("\n");
+    return 0;
+}
+/*
+ * Output:
+ * Enter number of elements: 7
+ * Enter 7 elements: 6 4 7 8 1 9 5
+ * Sorted array: 1 4 5 6 7 8 9
+ */
