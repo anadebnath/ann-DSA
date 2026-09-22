de3d578f5b0c81f44de26b3a565c052557f2cb06
Ana. <anamikadvnth80@gmail.com>
2026-09-22 19:45:29 +0530

    Correct merge function logic in mergeSort
    Fix the merge function to correctly copy elements from the original array and sort them.

--- 07. Sorting/22. mergeSort function.c
+++ 07. Sorting/22. mergeSort function.c
@@ -1,1 +1,62 @@
+/*
+ * Program No.: 22
+ * Program Name: Merge sort
+ * Date: 31.08.26
+ * Aim: Perform the merge-sort program listed as “Merge Sort Junction” in the index.
+ */
+#include <stdio.h>
 
+void merge(int arr[], int p, int q, int r) {
+    int n1 = q - p + 1;
+    int n2 = r - q;
+    int L[n1], M[n2];
+    
+    for (int i = 0; i < n1; i++) {
+        L[i] = arr[p + i];
+    }
+    for (int j = 0; j < n2; j++) {
+        M[j] = arr[q + 1 + j];
+    }
+    
+    int i = 0, j = 0, k = p;
+    while (i < n1 && j < n2) {
+        if (L[i] <= M[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = M[j];
+            j++;
+        }
+        k++;
+    }
+    
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    
+    while (j < n2) {
+        arr[k] = M[j];
+        j++;
+        k++;
+    }
+}
+
+int main() {
+    int arr[] = {3, 8, 9, 1, 5, 7};
+    merge(arr, 0, 2, 5);
+    
+    for (int i = 0; i < 6; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
+/*
+ * Output:
+ * Enter number of elements: 7
+ * Enter 7 elements: 38 27 43 3 9 82 10
+ * Sorted Array
+ * 3 9 10 27 38 43 82
+ */
