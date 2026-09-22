f2916215e388adea8fb4870f74e526a9c2eb0a46
Ana. <anamikadvnth80@gmail.com>
2026-09-22 19:04:36 +0530

    Implement memory address calculation for 1D array
    This program calculates the memory address of a one-dimensional array element based on user input for base address, index, and data type size.

--- 06. Memory-Addresssing/13. 1d_array_address.c
+++ 06. Memory-Addresssing/13. 1d_array_address.c
@@ -1,1 +1,30 @@
+/*
+ * Program No.: 13
+ * Program Name: Calculate 1D array element memory address
+ * Date: 17.08.26
+ * Aim: Calculate the memory address of a one-dimensional array element.
+ */
+#include <stdio.h>
 
+int main() {
+    int B, index, size_dt;
+    printf("Enter Base Address (B): ");
+    scanf("%d", &B);
+    printf("Enter index: ");
+    scanf("%d", &index);
+    printf("Enter the size of data type in bytes (size_dt): ");
+    scanf("%d", &size_dt);
+    
+    int address = B + (index * size_dt);
+    printf("The calculated address of the element is : %d\n", address);
+    
+    return 0;
+}
+/*
+ * Output:
+ * Enter Base Address (B): 1000
+ * Enter index: 4
+ * Enter the size of data type in bytes (w): 4
+ * Enter lower bound (LB): 0
+ * The calculated address of the element is: 1016
+ */
