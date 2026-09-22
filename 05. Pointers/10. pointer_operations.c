04f59b0717c5e67444606c41cd30c55d56503b27
Ana. <anamikadvnth80@gmail.com>
2026-09-22 18:59:30 +0530

    Implement pointer operations example in C
    Demonstrate basic pointer and pointer-to-pointer operations in C.

--- 05. Pointers/10. pointer_operations.c
+++ 05. Pointers/10. pointer_operations.c
@@ -1,1 +1,34 @@
+/*
+ * Program No.: 10
+ * Program Name: The pointers
+ * Date: 17.08.26
+ * Aim: Demonstrate basic pointer and pointer-to-pointer operations.
+ */
+#include <stdio.h>
 
+int main(void)
+{
+    int n = 50;
+    int value = 10;
+    int *pointer = &n;
+    int **pointer_to_pointer = &pointer;
+
+    printf("%d %d\n", n, value);
+    printf("%d\n", *pointer);
+
+    *pointer = 25;
+    printf("%d %d\n", n, *pointer);
+
+    **pointer_to_pointer = 50;
+    printf("%d %d %d\n", n, *pointer, **pointer_to_pointer);
+
+    return 0;
+}
+
+/*
+ * Output:
+ * 50 10
+ * 50
+ * 25 25
+ * 50 50 50
+ */
